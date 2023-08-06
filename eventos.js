4857f3cf8fcb8a0c39d7bb1f5dc6e58b2b6dd92f
magdalena65 <maguisuarezpereira@gmail.com>
2023-08-06 15:54:59 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const divboton = document.querySelector("#boton button");
     
     divboton.addEventListener("click", () => {
+        divboton.stopPropagation();
         alert("Hola! Soy el div");
     });
 })
